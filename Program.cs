c4bea33f34794917956b83d8a13b6c12df5fff08
Ysmael Braga <ysmael.braga@ibm.com>
2022-12-06 01:05:30 -0300

    listando as pastas existentes no projeto

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace DirectoryDirectoryInfo
+﻿
+
+using DirectoryDirectoryInfo;
+using DirectoryDirectoryInfo.files.notes;
+
+namespace DirectoryDirectoryInfo
 {
     class Program
     {
+     
         static void Main(string[] args)
         {
+          
+            try
+            {
+                PathFile path = new PathFile();
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path.UpdateSourcePath(), "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
 
+             
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("An error occureed");
+                Console.WriteLine(error.Message);
+            }
         }
     }
 }
